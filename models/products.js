da4c2e6d2f922006ebfef718c5193fa9541ca545
alohasunny <senneik@gmail.com>
2022-03-10 12:46:50 +1300

    Add products model and change artists to products

---
+++ models/products.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ProductsSchema = new Schema({
+  name: String,
+  description: String,
+  price: Number,
+  status: String,
+  thumb: String,
+});
+
+// singular capitalized name for the mongo collection - Products
+const Product = mongoose.model("Product", ProductsSchema);
+
+module.exports = Product;
